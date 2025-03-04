7c7a75ad9beae04f4e451a84bcbef03f7c07def5
mogumogu934 <107589445+mogumogu934@users.noreply.github.com>
2025-03-03 21:47:43 -0800

    Remove unneeded json fields

--- internal/pokeapi/location_resp.go
+++ internal/pokeapi/location_resp.go
@@ -12,41 +12,12 @@ import (
 )
 
 type LocationAreaResp struct {
-	ID       int `json:"id"`
-	Location struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"location"`
-	Name  string `json:"name"`
-	Names []struct {
-		Language struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"language"`
-		Name string `json:"name"`
-	} `json:"names"`
+	ID                int    `json:"id"`
+	Name              string `json:"name"`
 	PokemonEncounters []struct {
 		Pokemon struct {
 			Name string `json:"name"`
-			URL  string `json:"url"`
 		} `json:"pokemon"`
-		VersionDetails []struct {
-			EncounterDetails []struct {
-				Chance          int   `json:"chance"`
-				ConditionValues []any `json:"condition_values"`
-				MaxLevel        int   `json:"max_level"`
-				Method          struct {
-					Name string `json:"name"`
-					URL  string `json:"url"`
-				} `json:"method"`
-				MinLevel int `json:"min_level"`
-			} `json:"encounter_details"`
-			MaxChance int `json:"max_chance"`
-			Version   struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version"`
-		} `json:"version_details"`
 	} `json:"pokemon_encounters"`
 }
 
